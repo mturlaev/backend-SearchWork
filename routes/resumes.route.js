93ae1559078cdb594351d8eeab8f29127216ce89
Magomed-Emin <sadikov.magomed@bk.ru>
2022-07-26 22:27:01 +0300

    Добавил мидлвары для резюме

--- routes/resumes.route.js
+++ routes/resumes.route.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/resume", resumesController.getResume);
 router.get("/resume/category/:id", resumesController.getByCategories);
 router.post("/resume", authMiddleware,resumesController.postResume);
-router.patch("/resume/:id", resumesController.patchResume);
-router.delete("/resume/:id", resumesController.deleteResume);
+router.patch("/resume/:id",authMiddleware ,resumesController.patchResume);
+router.delete("/resume/:id", authMiddleware ,resumesController.deleteResume);
 
 module.exports = router;
