80e19eed36f014798b2cbe19a5fd78e7bc21a3eb
mturlaev <muhammadturlaev@gmail.com>
2022-07-26 23:30:47 +0300

    накидал бэк p.s не помню уже что менял

--- middlewares/file.js
+++ middlewares/file.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
 })
 
 const types = ["image/png", "image/jpeg", "image.jpg"];
-
+// dfkmfv
 const fileFilter = (req, file, cb) => {
     if(types.includes(file.mimetype)) {
         cb(null, true)
